a91ea53a216eb9976434b306c63ee421969dad9c
shubhadip <shubhadip.maity@gmail.com>
2023-09-27 11:17:06 +0530

    Update makeAdder.js

--- makeAdder.js
+++ makeAdder.js
@@ -1,23 +1,17 @@
-function makeAdder(...x) {
-  return function(...y) {
-    if(typeof x === 'object' && Array.isArray(y)) {
-      x = x.reduce((acc, init) => acc * init, 0)
+const sum = function (...args) {
+    let total = args.reduce((acc,init) => acc + init, 0)
+    function add(...args2) {
+        if(args2.length){
+            total = args2.reduce((acc,init) => acc + init, total)
+            return add
+        }
+        return total;
     }
-    if(typeof y === 'object' && Array.isArray(y)) {
-      y = y.reduce((acc, init) => acc * init, 0)
-    }
-    // x = x.reduce((acc, init) => acc * init, 0)
-    // y = y.reduce((acc, init) => acc * init, 0)
-   if(y){
-    return makeAdder(x * y);
-   }else{
-     return x
-   }
-  }
-};
 
-// console.log('makeAdder', makeAdder(1,4)(2,2)(3)());
-// console.log('makeAdder', makeAdder(1)(4)());
-// console.log('makeAdder', makeAdder(1,4)());
-console.log('makeAdder', makeAdder(1)(2)(2)());
-console.log('makeAdder', makeAdder()());
+    return add
+  }
+  
+
+console.log('sum', sum(1)(2)(2)());
+console.log('sum', sum(1,2)(2,3)(2,4)());
+console.log('sum', sum(1)(2)(2)());
