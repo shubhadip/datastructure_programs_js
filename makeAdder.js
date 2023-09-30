b74a244f985389ada06b86000d9e4865e616ae53
shubhadip <shubhadip.maity@gmail.com>
2023-09-30 08:27:52 +0530

    Update makeAdder.js

--- makeAdder.js
+++ makeAdder.js
@@ -15,3 +15,25 @@ const sum = function (...args) {
 console.log('sum', sum(1)(2)(2)());
 console.log('sum', sum(1,2)(2,3)(2,4)());
 console.log('sum', sum(1)(2)(2)());
+
+
+
+
+
+
+const parent = (fn1) => {
+    return function(...x) {
+        let total = x.reduce(fn1, 1)
+        function add(...y) {
+            if(y.length) {
+                total = y.reduce(fn1, total)
+                return add
+            }else {
+                return total;
+            }
+        }
+        return add
+    }
+}
+const fn1 = (acc,curr) => acc = acc*curr
+console.log(parent(fn1)(1,2)(2,3)());
